04bba5e6a0c87718c1af43b7141f9710a98f3753
Krissie Lauwers <kristinalauwers@gmail.com>
2021-07-16 12:05:06 -0400

    update msi script
    Now create the app image separately so that the command line util can be copied before packaging. Still need to add signing, and tts works when running the jar, but not the exe launcher.

--- Package/CreateWinMsi.bat
+++ Package/CreateWinMsi.bat
@@ -5,14 +5,11 @@ SET JMODS="C:\Program Files\javafx-jmods-16"
 
 SET FILESDIR=temp
 
-
 mkdir %filesdir%
 copy ..\BlueBirdConnector\out\artifacts\BlueBirdConnector_jar\BlueBirdConnector.jar %filesdir%\BlueBirdConnector.jar
 
-:: Make msi
-jpackage ^
-  --type msi ^
-  --app-version %ver% ^
+:: Make the app image separately so that the command line application can be copied in before the msi is made.
+jpackage --type app-image --app-version %ver% ^
   --copyright "BirdBrain Technologies LLC" ^
   --description "BlueBird Connector from BirdBrain Technologies" ^
   --name "BlueBird Connector" ^
@@ -22,10 +19,49 @@ jpackage ^
   --module-path %jmods% ^
   --add-modules javafx.controls,javafx.web ^
   --main-jar BlueBirdConnector.jar ^
-  --main-class com.birdbraintechnologies.bluebirdconnector.BlueBirdConnector ^
+  --main-class com.birdbraintechnologies.bluebirdconnector.BlueBirdConnector
+
+:: copy in the windows native bluetooth utility. This needs to be in the same folder as the launcher.
+copy ..\BlueBirdWindowsCL\bin\Release\BlueBirdWindowsCL.exe "BlueBird Connector\BlueBirdWindowsCL.exe"
+
+:: goto :eof
+
+:: Make msi
+jpackage ^
+  --type msi ^
+  --app-version %ver% ^
+  --copyright "BirdBrain Technologies LLC" ^
+  --description "BlueBird Connector from BirdBrain Technologies" ^
+  --name "BlueBird Connector" ^
+  --vendor "BirdBrain Technologies" ^
+  --app-image "BlueBird Connector" ^
   --win-menu ^
-  --win-shortcut ^
-  --win-console
+  --win-shortcut
 
 :: Cleanup
 rmdir %filesdir% /S /Q
+rmdir "BlueBird Connector" /S /Q
+
+
+:: NOTES:
+:: You can add additional launchers for debugging or for tts, but the installer
+:: will then install these with separate icons on the desktop.
+:: Example:
+:: first define variables
+:: SET DEBUGOPTIONSFILE=debug.properties
+:: SET TTSOPTIONSFILE=tts.properties
+:: Write the properties files:
+:: @echo off
+:: echo win-console=true> %DEBUGOPTIONSFILE%
+:: echo arguments=tts> %TTSOPTIONSFILE%
+:: echo win-console=true>> %TTSOPTIONSFILE%
+:: @echo on
+:: Then, add the following options when making the app-image:
+:: --add-launcher "BlueBird Connector Debug"=%DEBUGOPTIONSFILE% ^
+:: --add-launcher "BlueBird Connector TTS"=%TTSOPTIONSFILE% ^
+:: Cleanup:
+:: del %DEBUGOPTIONSFILE%
+:: del %TTSOPTIONSFILE%
+::
+:: TTS can be used by adding the command line argument 'tts' and optionally the
+:: additional argument 'autocalib' to autocalibrate the compass.
