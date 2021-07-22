ad83ccc85b466a1e5451a187d84177ba1b584be9
Krissie Lauwers <kristinalauwers@gmail.com>
2021-07-22 11:36:46 -0400

    Add bluegiga driver
    Add the bluegiga driver and add a custom action to install it to the msi. This still needs to be tested. Also, add the command line utility exe to the repo. It's small and probably won't need much modification.

--- Package/CreateWinMsi.bat
+++ Package/CreateWinMsi.bat
@@ -5,11 +5,17 @@ if [%1]==[] (
 )
 
 :: Set the current app version
-SET VER=3.0
+:: SET VER=3.0
+cd ..\BlueBirdConnector
+for /f %%i in ('call mvn -q --non-recursive "-Dexec.executable=cmd" "-Dexec.args=/C echo ${project.version}" "org.codehaus.mojo:exec-maven-plugin:1.3.1:exec"') do set VER=%%i
+echo Setting %ver% as the project version
+cd ..\Package
+
 :: Set the path to your copy of the javafx jmods
 SET JMODS="C:\Program Files\javafx-jmods-16"
 
 SET FILESDIR=temp
+SET SUPPORTDIR=supportFiles
 
 echo Copying the jar file...
 mkdir %filesdir%
@@ -17,28 +23,36 @@ copy ..\BlueBirdConnector\out\artifacts\BlueBirdConnector_jar\BlueBirdConnector.
 
 :: Make the app image separately so that the command line application can be copied in before the msi is made.
 echo Creating the app-image...
-jpackage --type app-image --app-version %ver% ^
+jpackage ^
+  --type app-image ^
+  --app-version %ver% ^
   --copyright "BirdBrain Technologies LLC" ^
   --description "BlueBird Connector from BirdBrain Technologies" ^
   --name "BlueBird Connector" ^
   --vendor "BirdBrain Technologies" ^
   --input %filesdir% ^
-  --icon hummingbirdlogo.ico ^
+  --icon %SUPPORTDIR%\hummingbirdlogo.ico ^
   --module-path %jmods% ^
   --add-modules javafx.controls,javafx.web ^
   --main-jar BlueBirdConnector.jar ^
+  --verbose ^
   --main-class com.birdbraintechnologies.bluebirdconnector.BlueBirdConnector
+:: Additional useful flags:
+:: * Have the app run in a console window (good for debugging) with
+::  --win-console ^
 
 :: copy in the windows native bluetooth utility. This needs to be in the same folder as the launcher.
 echo Copying the command line utility...
 copy ..\BlueBirdWindowsCL\bin\Release\BlueBirdWindowsCL.exe "BlueBird Connector\BlueBirdWindowsCL.exe"
+copy %SUPPORTDIR%\usbserial.cat "BlueBird Connector\usbserial.cat"
+copy %SUPPORTDIR%\usbserial.inf "BlueBird Connector\usbserial.inf"
 
-echo Signing BlueBirdConnector.jar, BlueBird Connector.exe, and BlueBirdWindowsCL.exe...
-jarsigner -storetype pkcs12 -keystore BIRDBRAIN.pfx -storepass %1 "BlueBird Connector\app\BlueBirdConnector.jar" 73cfaf53eaee4153b44e02ca7b2a7e76
-attrib -r "BlueBird Connector\BlueBird Connector.exe"
-signtool sign /fd SHA256 /f BIRDBRAIN.pfx /p %1 "BlueBird Connector\BlueBird Connector.exe"
-attrib +r "BlueBird Connector\BlueBird Connector.exe"
-signtool sign /fd SHA256 /f BIRDBRAIN.pfx /p %1 "BlueBird Connector\BlueBirdWindowsCL.exe"
+::echo Signing BlueBirdConnector.jar, BlueBird Connector.exe, and BlueBirdWindowsCL.exe...
+::jarsigner -storetype pkcs12 -keystore BIRDBRAIN.pfx -storepass %1 "BlueBird Connector\app\BlueBirdConnector.jar" 73cfaf53eaee4153b44e02ca7b2a7e76
+::attrib -r "BlueBird Connector\BlueBird Connector.exe"
+::signtool sign /fd SHA256 /f BIRDBRAIN.pfx /p %1 "BlueBird Connector\BlueBird Connector.exe"
+::attrib +r "BlueBird Connector\BlueBird Connector.exe"
+::signtool sign /fd SHA256 /f BIRDBRAIN.pfx /p %1 "BlueBird Connector\BlueBirdWindowsCL.exe"
 
 ::goto :eof
 
@@ -52,11 +66,17 @@ jpackage ^
   --name "BlueBird Connector" ^
   --vendor "BirdBrain Technologies" ^
   --app-image "BlueBird Connector" ^
+  --verbose ^
+  --resource-dir overrides ^
   --win-menu ^
   --win-shortcut
+:: Additional useful flags:
+:: * see copies of the temp files being used with
+::  --temp tempFiles ^
+::
 
-echo Signing the .msi...
-signtool sign /fd SHA256 /f BIRDBRAIN.pfx /p %1 "BlueBird Connector-%ver%.msi"
+::echo Signing the .msi...
+::signtool sign /fd SHA256 /f BIRDBRAIN.pfx /p %1 "BlueBird Connector-%ver%.msi"
 
 :: Cleanup
 echo Cleaning up...
