f574c88751cb3862036475bfd5b2b16906eb9334
Kevin Walsh <kwalsh@holycross.edu>
2025-08-15 22:44:05 -0400

    Include dbus modules in distribution
    The dbus modules must be runtime-discoverable in the jlink distribution.

--- BlueBirdConnector/src/main/java/module-info.java
+++ BlueBirdConnector/src/main/java/module-info.java
@@ -11,6 +11,7 @@ module BlueBirdConnector {
     requires java.logging;
     requires freetts;
     requires org.freedesktop.dbus;
+    requires org.freedesktop.dbus.transport.jre;
 
     exports com.birdbraintechnologies.bluebirdconnector;
 }
