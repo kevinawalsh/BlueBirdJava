30877fe82ea1cd585a3499d5a0baba3e1ce40e3b
Kevin Walsh <kwalsh@holycross.edu>
2025-08-16 20:54:51 -0400

    Include dependency needed by jetty

--- BlueBirdConnector/src/main/java/module-info.java
+++ BlueBirdConnector/src/main/java/module-info.java
@@ -3,10 +3,11 @@ module BlueBirdConnector {
     requires javafx.web;
     requires jdk.jsobject;
     requires com.google.gson;
-    requires slf4j.nop;
+    requires slf4j.nop; // used to silence jetty slf4j log messages
     requires org.eclipse.jetty.server;
     requires org.eclipse.jetty.servlet;
     requires org.eclipse.jetty.servlets;
+    requires java.management; // needed by jetty, queried with runtime reflection
     requires java.desktop;
     requires com.fazecast.jSerialComm;
     requires java.logging;
